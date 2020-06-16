378c5a8cc2b3b259f6af013a279eb9c9895de102
LMFriesen <59742083+LMFriesen@users.noreply.github.com>
2020-06-15 19:20:10 -0600

    2020-06-15-7:20PM

--- app/src/main/java/ca/nait/dmit2504/oscarreviews/ViewReviewsActivity.java
+++ app/src/main/java/ca/nait/dmit2504/oscarreviews/ViewReviewsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +41,7 @@ public class ViewReviewsActivity extends AppCompatActivity {
                 // Convert the single string of values into a list of values
                 List<Jitter> jittersList = new ArrayList<>();
                 String[] jittersArray = responseBody.split("\r\n");
-                for (String jitterText : jittersArray) {
+                /*for (String jitterText : jittersArray) {
                     Jitter currentJitter = new Jitter();
                     String date = jitterText.substring(0, jitterText.indexOf(" *-*-* "));
                     String reviewer = jitterText.substring(1, jitterText.indexOf("\n"));
@@ -53,11 +55,36 @@ public class ViewReviewsActivity extends AppCompatActivity {
                     currentJitter.setReview(review);
                     // add currentJitter to jittersList
                     jittersList.add(currentJitter);
+                }*/
+                try {
+                    BufferedReader reader = new BufferedReader(new StringReader(responseBody));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        Jitter currentJitter = new Jitter();
+                        String date = line;
+                        String reviewer = reader.readLine();
+                        String category = reader.readLine();
+                        String nominee = reader.readLine();
+                        String review = reader.readLine();
+                        currentJitter.setDate(date);
+                        currentJitter.setReviewer(reviewer);
+                        currentJitter.setCategory(category);
+                        currentJitter.setNominee(nominee);
+                        currentJitter.setReview(review);
+                        // add currentJitter to jittersList
+                        jittersList.add(currentJitter);
+                    }
+
+
+                    // Find the ListView and set the adapter for the listview
+                    ListView jittersListView = findViewById(R.id.activity_custom_list_view_jitters_listview);
+                    JittersListViewAdapter jitterAdapter = new JittersListViewAdapter(getApplicationContext(), jittersList);
+                    jittersListView.setAdapter(jitterAdapter);
                 }
-                // Find the ListView and set the adapter for the listview
-                ListView jittersListView = findViewById(R.id.activity_custom_list_view_jitters_listview);
-                JittersListViewAdapter jitterAdapter = new JittersListViewAdapter(getApplicationContext(), jittersList);
-                jittersListView.setAdapter(jitterAdapter);
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Fetch jitters was not successful.", Toast.LENGTH_SHORT).show();
+                }
             }
             @Override
             public void onFailure(final Call<String> call, final Throwable t) {
